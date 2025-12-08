f0fb96c70bb39db7b03812c26fcbe4cacd62740f
marcushe13 <Marcusrhernandez99@gmail.com>
2025-12-07 23:50:27 -0800

    FINAL COMMIT

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        // ============ Part A: Create the Window =============
         JFrame frame = new JFrame("Boba n Chill");
         frame.setSize(700, 645);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        // Card Layout Setup (to switch screens)
+        // Card Layout Setup that switches screens)
         CardLayout cardLayout = new CardLayout();
         JPanel mainPanel = new JPanel(cardLayout);
 
         // HOME SCREEN
         JPanel homePanel = new JPanel(new BorderLayout());
-
         JLabel title = new JLabel("Welcome to Boba n Chill", SwingConstants.CENTER);
         title.setFont(new Font("Arial", Font.BOLD, 26));
         homePanel.add(title,BorderLayout.NORTH);
 
+        //Start Buttons
         JButton startButton = new JButton("Start your order!");
         startButton.setFont(new Font("Arial", Font.BOLD, 20));
         homePanel.add(startButton, BorderLayout.SOUTH);
 
-        // adding image
+        // adding homescreen boba image (boba.png)
         ImageIcon boba = new ImageIcon("boba.png");
         JLabel imageLabel = new JLabel(boba);
         imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         homePanel.add(imageLabel, BorderLayout.CENTER);
 
-        // ORDER SCREEN
+        // ORDER SCREEN setup
         JPanel orderPanel = new JPanel();
         orderPanel.setLayout(new GridLayout(3,2,8,8));
-
         Font buttonFont = new Font("Arial" , Font.BOLD, 14);
 
-        // New order list display
+        // ORDER list display (on the right sidebar)
         JTextArea orderList = new JTextArea();
         orderList.setEditable(false);
         orderList.setFont(new Font("Arial", Font.BOLD, 14));
-
         final double[] totalPrice = {0};
 
-        JScrollPane scroll = new JScrollPane(orderList);
+        // new file that stores the order history
+        String historyFile = "order_history.txt";
 
-        //buttons
+        //DRINK BUTTONS (creating)
         JButton taro = new JButton("Taro Milk Tea - $5.50");
         taro.setFont(buttonFont);
-
         JButton thai = new JButton("Thai Tea - $5.00");
         thai.setFont(buttonFont);
-
         JButton matcha = new JButton("Matcha Latte - $6.00");
         matcha.setFont(buttonFont);
-
         JButton sugar = new JButton("Brown Sugar Milk Tea - $6.50");
         sugar.setFont(buttonFont);
-
         JButton viet = new JButton("Vietnamese Coffee - $5.75");
         viet.setFont(buttonFont);
-
         JButton oolong = new JButton("Oolong Milk Tea - $5.25");
-        oolong.setFont(buttonFont);
 
+        oolong.setFont(buttonFont);
         orderPanel.add(taro);
         orderPanel.add(thai);
         orderPanel.add(matcha);
@@ -72,19 +65,14 @@ public class Main {
         orderPanel.add(viet);
         orderPanel.add(oolong);
 
-        // adding panels to cardlayout
+        // adding panels to cardlayout (also wraps the 6 buttons)
         mainPanel.add(homePanel, "HOME");
-
-
         JPanel orderWrapper = new JPanel(new BorderLayout());
         orderWrapper.add(orderPanel, BorderLayout.CENTER);
-
         JPanel orderScreen = new JPanel(new BorderLayout());
         mainPanel.add(orderScreen, "ORDER");
         orderScreen.add(orderWrapper, BorderLayout.CENTER);
         orderScreen.add(new JScrollPane(orderList), BorderLayout.EAST);
-        orderScreen.revalidate();
-
         JPanel bottomBar = new JPanel();
 
         // place order button
@@ -95,83 +83,63 @@ public class Main {
         JButton history = new JButton("ORDER HISTORY");
         history.setFont(buttonFont);
 
-        // remove last BUTTON
+        // remove last drink button
         JButton undo = new JButton("REMOVE LAST ITEM");
         undo.setFont(buttonFont);
 
-        // total LABEL
+        // total LABEL (on the bottom right)
         JLabel total = new JLabel("Total: $0.00");
         total.setFont(buttonFont);
 
-        // adding components to bottom bar
+        // adding components (above) to bottom bar
         bottomBar.add(undo);
         bottomBar.add(placeOrder);
         bottomBar.add(history);
         bottomBar.add(total);
-
-        orderScreen.add(bottomBar, BorderLayout.SOUTH);
+        orderScreen.add(bottomBar, BorderLayout.SOUTH); //allignment
 
         // add mainPanel to frame
         frame.add(mainPanel, BorderLayout.CENTER);
 
-        // switch screens when "ORDER" button is pressed
+        // switch screens when "ORDER" button is pressed (from home to order screen)
         startButton.addActionListener(e -> {
             cardLayout.show(mainPanel, "ORDER");
         });
-
         taro.addActionListener(e-> {
             orderList.append("Taro Milk Tea - $5.50 \n");
             totalPrice[0] += 5.50;
             total.setText("Total: $" + String.format("%.2f", totalPrice[0]));
-            frame.revalidate();
-            frame.repaint();
-
         });
-
         thai.addActionListener(e -> {
             orderList.append("Thai Tea - $5.00\n");
             totalPrice[0] += 5.00;
             total.setText("Total: $" + String.format("%.2f", totalPrice[0]));
-            frame.revalidate();
-            frame.repaint();
         });
-
         matcha.addActionListener(e -> {
             orderList.append("Matcha Latte - $6.00\n");
             totalPrice[0] += 6.00;
             total.setText("Total: $" + String.format("%.2f", totalPrice[0]));
-            frame.revalidate();
-            frame.repaint();
         });
-
         sugar.addActionListener(e -> {
             orderList.append("Brown Sugar Milk Tea - $6.50\n");
             totalPrice[0] += 6.50;
             total.setText("Total: $" + String.format("%.2f", totalPrice[0]));
-            frame.revalidate();
-            frame.repaint();
         });
-
         viet.addActionListener(e -> {
             orderList.append("Vietnamese Coffee - $5.75\n");
             totalPrice[0] += 5.75;
             total.setText("Total: $" + String.format("%.2f", totalPrice[0]));
-            frame.revalidate();
-            frame.repaint();
         });
-
         oolong.addActionListener(e -> {
             orderList.append("Oolong Milk Tea - $5.25\n");
             totalPrice[0] += 5.25;
             total.setText("Total: $" + String.format("%.2f", totalPrice[0]));
-            frame.revalidate();
-            frame.repaint();
         });
 
         // PLACE ORDER BUTTON ACTION LISTENER
-
-        int orderNumber = (int)(Math.random() * 100 ) + 1;
         placeOrder.addActionListener(e -> {
+            int orderNumber = (int)(Math.random() * 100 ) + 1;
+            String currentOrderText = orderList.getText();
             JOptionPane.showMessageDialog(frame,
                     "----- Your Order ----- \n\n"
                             + "Order Number: #" + orderNumber
@@ -181,9 +149,21 @@ public class Main {
                             + String.format("%.2f", totalPrice[0])
                     + "\n\n Thank you for \n choosing Boba N Chill! \n"
                     + "----------------------------",
-                    "Order Receipt", JOptionPane.INFORMATION_MESSAGE);
+                    "Order Receipt", JOptionPane.INFORMATION_MESSAGE); //thisis the receipt popup window
 
-            // RESTS ALL OF THE TEXT and TOTAL
+            // adds order to the history file
+           try (FileWriter fw = new FileWriter(historyFile, true);
+                BufferedWriter bw = new BufferedWriter(fw)) {
+               bw.write("\n Order Number: #" + orderNumber + "\n");
+               bw.write(currentOrderText);
+               bw.write("TOTAL: $" + String.format("%.2f", totalPrice[0]) + "\n");
+               bw.write("---------------------------- \n \n");
+           } catch (IOException ex) {
+               JOptionPane.showMessageDialog(frame,
+                       "Error Saving order history.", "Error", JOptionPane.ERROR_MESSAGE); //error fail
+           }
+
+            // RESETS ALL OF THE TEXT and TOTAL $
             orderList.setText("");
             totalPrice[0] = 0;
             total.setText("Total: $0.00");
@@ -192,26 +172,55 @@ public class Main {
         // REMOVE BUTTON ACTION LISTENER
         undo.addActionListener(e-> {
             String text = orderList.getText().trim();
-
             if(text.isEmpty()) return;
-
             String[] lines = text.split("\n");
             String last = lines [lines.length - 1];
-
             orderList.setText(text.substring(0, text.lastIndexOf("\n")));
 
             // updates total price depending on price of last
-            if (last.contains("5.50")) totalPrice[0] -= 5.50;
+            if (last.contains("5.50")) totalPrice[0] -= 5.50; // try to update? must be a more efficient way
             else if (last.contains("5.00")) totalPrice[0] -= 5.00;
             else if (last.contains("6.00")) totalPrice[0] -= 6.00;
             else if (last.contains("6.50")) totalPrice[0] -= 6.50;
             else if (last.contains("5.75")) totalPrice[0] -= 5.75;
             else if (last.contains("5.25")) totalPrice[0] -= 5.25;
-
             total.setText("Total: $" + String.format("%.2f", totalPrice[0]));
-
         });
 
-        frame.setVisible(true);
+        history.addActionListener(e -> {
+            File file = new File(historyFile);
+
+            // checking to see if file doesn't exist OR if the file is empty (==0)
+            if (!file.exists() || file.length() == 0) {
+                JOptionPane.showMessageDialog(frame,
+                        "No previous orders yet.","Order History", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+
+            StringBuilder historyText = new StringBuilder();
+            // br reads file line by line UNTIL THERE ARE NO MORE LINES, then it will add each line to stringBuilder**
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    historyText.append(line).append("\n");
+                }
+            } catch (IOException ex) { //fail
+                JOptionPane.showMessageDialog(frame,
+                        "Error reading order history.", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            JTextArea historyArea = new JTextArea(historyText.toString());
+            historyArea.setEditable(false); // a USER CANNOT type inside of it, they can only read it
+            historyArea.setFont(new Font("Arial", Font.BOLD, 12));
+
+            // scrolling pane in case the history extends (long history)
+            JScrollPane historyScroll = new JScrollPane(historyArea);
+            historyScroll.setPreferredSize(new Dimension(400,300)); //sizeof popup window
+
+            // this is the popup window that contains the order history
+            JOptionPane.showMessageDialog(frame,
+                    historyScroll,"Order History", JOptionPane.INFORMATION_MESSAGE);
+        });
+        frame.setVisible(true); //visibility = true
     }
 }
